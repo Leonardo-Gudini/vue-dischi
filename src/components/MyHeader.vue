2caf8c6b54f6520fda7920e01345cc35020cbe8c
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-08-28 15:07:12 +0200

    add header and main style, add call axios

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <div class="logo-container">
+        <img class="logo" src="../assets/logo.png" alt="logo">
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "MyHeader.vue"
+}
+</script>
+
+<style lang="scss">
+    @import "../style/headerStyle.scss";
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,39 @@
+<template>
+  <main>
+
+        <div class="my-container container text-center">
+            <div class="row">
+                <div class="col">ciao</div>
+            </div>
+        </div>
+  </main>
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+    name: "MyMain",
+
+    data() {
+       return{
+         musicDiskList : []
+       }
+    },
+
+    created(){
+
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then( response => {
+            this.musicDiskList = response.data.response;
+        })
+    }
+}
+</script>
+
+<style>
+
+@import "../style/mainStyle.scss"
+
+</style>
