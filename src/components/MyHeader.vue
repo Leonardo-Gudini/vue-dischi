b430ffa9d95dff10db128d011404c56fb808c01f
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-08-29 17:25:30 +0200

    add select

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,15 +1,25 @@
 <template>
-  <header>
+  <header class="d-flex justify-content-between align-items-center px-3">
     <div class="logo-container">
         <img class="logo" src="../assets/logo.png" alt="logo">
+    </div>
+
+    <div class="search">
+      <select class="form-select" aria-label="Default select example">
+        <option selected>Seleziona genere</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
     </div>
   </header>
 </template>
 
 <script>
 export default {
-    name: "MyHeader.vue"
+    name: "MyHeader.vue",
 }
+
 </script>
 
 <style lang="scss">
